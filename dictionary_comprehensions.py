71bfdc6321e697d12c82fb09df74b5635d7483ca
Sergio Garnica <sergio190201@gmail.com>
2021-04-30 06:12:13 -0500

    Actualizado dictionry_comprehensions

--- dictionary_comprehensions.py
+++ dictionary_comprehensions.py
@@ -1,8 +1,18 @@
+import math
+
+
 def run():
-    my_dictionary = {}
-    for i in range(1, 101):
-        my_dictionary[i] = i**3
-    print(my_dictionary)
+    # my_dictionary = {}
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         my_dictionary[i] = i**3
+
+    # my_dictionary = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+
+    # print(my_dictionary)
+
+    my_new_dictionary = {i: math.sqrt(i) for i in range(1, 1001)}
+    print(my_new_dictionary)
 
 
 if __name__ == '__main__':
